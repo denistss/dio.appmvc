8f0972921afe39ad166be66fbe0d35490d872ca5
Denis Tiago Silva <56735135+denistss@users.noreply.github.com>
2021-04-03 19:34:56 -0300

    Implementação de Produtos; DataAnotation Display/Descrição e Range/Quantidade;

--- CursoMVC/Models/Categoria.cs
+++ CursoMVC/Models/Categoria.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,8 @@ namespace CursoMVC.Models
     {
 
         public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage ="O campo descrição é obrigatório")]
         public string Descricao { get; set; }
     }
 }
